0012c66e3d523f801fdabb94cfd8e197be4978bc
ashley-colbert <136830986+ashley-colbert@users.noreply.github.com>
2024-05-29 11:21:51 -0230

    small changes

--- src/main/java/com/keyin/SuggestionEngine.java
+++ src/main/java/com/keyin/SuggestionEngine.java
@@ -90,16 +90,16 @@ public class SuggestionEngine {
 
     public static void main(String[] args) throws Exception {
         if (args.length != 1) {
-            System.out.println("USAGE: " + SuggestionEngine.class.getName() + " <word to generateSuggestions>");
+            System.out.println("USAGE: " + SuggestionEngine.class.getName() + " abot");
         }
         if (args.length == 1 && args[0].equalsIgnoreCase("?")) {
-            System.out.println("USAGE: " + SuggestionEngine.class.getName() + " <word to generateSuggestions>");
+            System.out.println("USAGE: " + SuggestionEngine.class.getName() + " abot");
             System.out.println("Output: A list of suggestions OR empty string if word is generateSuggestions");
         }
 
         SuggestionEngine suggestionEngine = new SuggestionEngine();
         suggestionEngine.loadDictionaryData(Paths.get( ClassLoader.getSystemResource("words.txt").getPath()));
 
-        System.out.println(suggestionEngine.generateSuggestions(args[0]));
+        System.out.println(suggestionEngine.generateSuggestions("abot"));
     }
 }
